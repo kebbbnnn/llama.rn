56a85d6b60428b2bf44f3fd03fb2595fe0ffa1db
Kevin Ladan <kevinladan@outlook.com>
2026-09-03 12:19:34 +0800

    fix(windows): set _ITERATOR_DEBUG_LEVEL=0 to match prebuilt rnllama.lib

--- windows/pch.h
+++ windows/pch.h
@@ -2,6 +2,11 @@
 
 #include "targetver.h"
 
+// Match the iterator debug level of prebuilt Release rnllama.lib
+#ifndef _ITERATOR_DEBUG_LEVEL
+#define _ITERATOR_DEBUG_LEVEL 0
+#endif
+
 #define NOMINMAX 1
 #define WIN32_LEAN_AND_MEAN 1
 #define WINRT_LEAN_AND_MEAN 1
